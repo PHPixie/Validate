eaf02f3a4ba1390b33fe18016cab94b32683fc6d
Dracony <draconyster@gmail.com>
2013-01-23 18:34:12 +0200

    Validate class finished

---
+++ validate/config/validate.php
@@ -0,0 +1,14 @@
+<?php
+return array(
+	'rules' => array(
+		/*You can declare your own rules here like this:
+		
+		//This would check if the value consists of uppercase
+		//letters only
+		'uppercase' => function($val) {
+			return (bool)preg_match('#[A-Z]#',$val);
+		}
+		*/
+		
+	)
+);
